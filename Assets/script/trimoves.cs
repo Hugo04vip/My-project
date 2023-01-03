757dde56f208154df754596d87b9632119b3fb98
Hugo04vip <113622776+Hugo04vip@users.noreply.github.com>
2023-01-03 11:41:56 +0100

    Initial commit

---
+++ Assets/script/trimoves.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trimoves : MonoBehaviour
+{
+    public int speed;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.trimoves(transform.position, Vector3.back speed * Time.deltaTime);
+    }
+}
