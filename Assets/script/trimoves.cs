903b848f773ab89a6763e85e87a167d69fab901a
Hugo04vip <113622776+Hugo04vip@users.noreply.github.com>
2023-01-03 13:57:17 +0100

    t2

--- Assets/script/trimoves.cs
+++ Assets/script/trimoves.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class trimoves : MonoBehaviour
 {
-    public int speed;
+    public float speed = 100f;
+    public int vidas = 0;
     
     void Start()
     {
-        
+        vidas+=1;
     }
 
     void Update()
     {
-        transform.trimoves
+        
+        if(Input.GetKey(KeyCode.A))
+            transform.Rotate(0f, 0f, speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(0f, 0f, speed * -1 * Time.deltaTime);
+        
     }
 }
